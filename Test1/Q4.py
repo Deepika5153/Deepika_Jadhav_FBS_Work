5f660f278e7012fc14290afbc3bb13fbef6184a9
Deepika5153 <deepikamagdum65@gmail.com>
2025-08-26 11:50:43 +0530

    Update Q4.py

--- Test1/Q4.py
+++ Test1/Q4.py
@@ -3,12 +3,13 @@ Interiorcost = int(input('Enter cose of Interior Wall painting : '))
 Exteriorcost = int(input('Enter cose of Exterior Wall painting : '))
 
 Interior_Area = areaOfWall * 8
-Interior_cost = Interior_Area * Interiorcost
-print(f'Cost of Paiting Interior Wall of Given diagram is {Interior_cost}')
+Total_Interior_cost = Interior_Area * Interiorcost
+print(f'Cost of Paiting Interior Wall of Given diagram is {Total_Interior_cost}')
 
 Exterior_Area = areaOfWall * 7
-Exterior_cost = Exterior_Area * Exteriorcost
-print(f'Cost of Paiting Exterior Wall of Given diagram is {Exterior_cost}')
+Total_Exterior_cost = Exterior_Area * Exteriorcost
+print(f'Cost of Paiting Exterior Wall of Given diagram is {Total_Exterior_cost}')
 
-Total_cost = Interior_cost + Exterior_cost
+Total_cost = Total_Interior_cost + Total_Exterior_cost
+
 print(f'Total Cost of Paiting of Given diagram is {Total_cost}')
